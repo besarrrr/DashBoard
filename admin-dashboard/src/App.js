e7aab5102d940422ea36b00becdb5daef087d846
Cesar Gonzalez <gcesar13@yahoo.com>
2022-10-20 23:35:07 -0600

    topbar and some css styling

--- admin-dashboard/src/App.js
+++ admin-dashboard/src/App.js
@@ -1,11 +1,24 @@
+import { ColorModeContext, useMode} from './theme';
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { Routes, Route } from "react-router-dom";
+import TopBar from "./scenes/global/TopBar"
+import Dashboard from "./scenes/dashboard"
 
 
 function App() {
-  return (
+  const [theme, colorMode] = useMode();
+ 
+  return ( <ColorModeContext.Provider value={colorMode}>
+    <ThemeProvider theme={theme}>
+    <CssBaseline />
     <div className="app">
-   
+    <main className="content">
+      <TopBar/>
+    </main>
     </div>
-  );
+    </ThemeProvider>
+    </ColorModeContext.Provider>
+    );
 }
 
 export default App;
